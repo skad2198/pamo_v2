820c5ac4cffb423223b6e9c7e32c4e97d35d1e65
skad2198 <50555846+skad2198@users.noreply.github.com>
2020-06-10 00:22:16 +0530

    cars.html

--- pamo/web/urls.py
+++ pamo/web/urls.py
@@ -8,4 +8,5 @@ app_name = 'web'
 urlpatterns = [
     path('', views.index, name="index"),
     # path('admin/', admin.site.urls),
+    path('cars/', views.cars_page, name='cars'),
 ]
--- pamo/web/views.py
+++ pamo/web/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
-
+from .models import vehicle
 # Create your views here.
 
 
 def index(request):
     return render(request, 'web/index.html')
+
+
+def cars_page(request, pg=1):
+
+    # Each page has 9 requests. That is fixed.
+    start = (pg-1) * 9
+    end = start + 9
+
+    car_list = vehicle.objects.all()[start:end]
+    context = {
+        'cars': car_list
+    }
+
+    return render(request, 'web/cars.html', context)
