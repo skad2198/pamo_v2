5d41ab6a434f65dda3506002a314736877a3f154
skad2198 <50555846+skad2198@users.noreply.github.com>
2020-06-09 23:42:04 +0530

    new vehicle model

--- pamo/web/models.py
+++ pamo/web/models.py
@@ -6,11 +6,13 @@ from django.contrib.auth.models import User
 
 class vehicle(models.Model):
 
+    image = models.FileField(null=True)
     name = models.CharField(max_length=100, default="abc")
     brand = models.CharField(max_length=20, choices=(('tata', 'TATA'), ('mahindra',
                                                                         'Mahindra'), ('daewoo', 'Daewoo')))
     vehicle_type = models.CharField(max_length=30, choices=(
         ('scv', "SCV"), ('pickup', "Pick-Up"), ('truck', "Truck"), ('bus', "Bus"), ('car', "Car"), ('tractor', "Agriculture Tractor"), ('others', "Others")), default="others")
+    description = models.TextField(max_length=2000, default="")
     added_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
