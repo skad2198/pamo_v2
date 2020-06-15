7e164fc72d7b92964244a9c3cf2e914cda14fc3f
skad2198 <50555846+skad2198@users.noreply.github.com>
2020-06-15 17:03:48 +0530

    yeet

--- pamo/web/urls.py
+++ pamo/web/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from . import views
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 app_name = 'web'
@@ -9,4 +11,4 @@ urlpatterns = [
     path('', views.index, name="index"),
     # path('admin/', admin.site.urls),
     path('cars/', views.cars_page, name='cars'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pamo/web/views.py
+++ pamo/web/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from .models import vehicle
+from django.db.models import Q
+from django.http import HttpResponse, request
+from django.core import serializers
 # Create your views here.
 
 
@@ -11,8 +14,8 @@ def index(request):
 def cars_page(request, pg=1):
 
     # Each page has 9 requests. That is fixed.
-    start = (pg-1) * 6
-    end = start + 6
+    start = (pg-1) * 9
+    end = start + 9
 
     car_list = vehicle.objects.all()[start:end]
     context = {
