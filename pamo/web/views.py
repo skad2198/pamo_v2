29b11210eaf23af9728b7852ade1b34a5fca2e19
skad2198 <50555846+skad2198@users.noreply.github.com>
2020-06-15 20:53:36 +0530

    views.py

--- pamo/web/views.py
+++ pamo/web/views.py
@@ -13,48 +13,36 @@ def index(request):
 
 
 def cars_page(request):
-    #print('I am here ')
     if request.GET:
+
         search_term = request.GET['search']
-        #search_term = search
-        #print('I am here in IF ')
-        # print(search_term)
+        print('I am here in IF ')
+        print(search_term)
         search_results = vehicle.objects.filter(
             Q(name__icontains=search_term) |
             Q(brand__icontains=search_term) |
             Q(description__icontains=search_term) |
             Q(vehicle_type__icontains=search_term)
         )
-
         paginator = Paginator(search_results, 9)  # Show 9 contacts per page.
 
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
-
         error_message = "Here's your search result for : " + search_term
-
         context = {
             'error_message': error_message,
             'search_term': search_term,
-            'page_obj': page_obj,
+            'page_obj': page_obj
         }
         return render(request, 'web/cars.html', context)
+
     else:
         vehicle_list = vehicle.objects.all()
         paginator = Paginator(vehicle_list, 9)  # Show 9 contacts per page.
 
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
-
-        #car_list = vehicle.objects.all()
-        #print('I am here in else')
-        error_message = ''
-        context = {
-            'error_message': error_message,
-            'page_obj': page_obj,
-        }
-        return render(request, 'web/cars.html', context)
-    return render(request, 'web/cars.html', {'page_obj': page_obj})
+        return render(request, 'web/cars.html', {'page_obj': page_obj})
 
 
 # def cars_page(request, pg=1):
@@ -79,3 +67,34 @@ def cars_details(request, car_id):
         'car_detail': car_detail,
     }
     return render(request, 'web/cars_details.html', context)
+
+
+# def search_cars(request):
+#     print('I am here ')
+#     if request.GET:
+#         search_term = request.GET['search']
+#         #search_term = search
+#         print('I am here in IF ')
+#         print(search_term)
+#         search_results = vehicle.objects.filter(
+#             Q(name__icontains=search_term) |
+#             Q(brand__icontains=search_term) |
+#             Q(description__icontains=search_term) |
+#             Q(vehicle_type__icontains=search_term)
+#         )
+#         error_message = "Here's your search result for : " + search_term
+#         context = {
+#             'error_message': error_message,
+#             'search_term': search_term,
+#             'page_obj': search_results
+#         }
+#         return render(request, 'web/cars.html', context)
+#     else:
+#         car_list = vehicle.objects.all()
+#         print('I am here in else')
+#         error_message = 'Nothing realted to your search'
+#         context = {
+#             'error_message': error_message,
+#             'page_obj': car_list,
+#         }
+#         return render(request, 'web/cars.html', context)
